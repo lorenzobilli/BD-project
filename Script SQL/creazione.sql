36c49aefa367c60c407c9ba130b10bcf5957e6b8
Lorenzo Billi <lorenzoaebilli@gmail.com>
2022-06-11 15:26:30 +0200

    Modified creation SQL script

--- Script SQL/creazione.sql
+++ Script SQL/creazione.sql
@@ -1,81 +1,80 @@
 CREATE SCHEMA IF NOT EXISTS sfide_online;
 SET SCHEMA 'sfide_online';
 
+CREATE TABLE IF NOT EXISTS set_icone
+(
+    nome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (nome)
+);
+
 CREATE TABLE IF NOT EXISTS gioco
 (
     id SERIAL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS set_icone
-(
-    nome VARCHAR(50) NOT NULL,
-    id_gioco SERIAL,
-    PRIMARY KEY (nome),
-    FOREIGN KEY (id_gioco) REFERENCES gioco(id)
+    num_dadi INT NOT NULL,
+    val_min_dadi INT NOT NULL,
+    val_max_dadi INT NOT NULL,
+    nome_set_icone VARCHAR(100) NOT NULL,
+    CONSTRAINT num_dadi_min CHECK (num_dadi >= 1),
+    CONSTRAINT val_dadi_min CHECK (val_min_dadi BETWEEN 0 AND 5),
+    CONSTRAINT val_dadi_max CHECK (val_max_dadi BETWEEN 1 AND 6),
+    CHECK (val_min_dadi <= val_max_dadi),
+    PRIMARY KEY (id),
+    FOREIGN KEY (nome_set_icone) REFERENCES set_icone(nome)
 );
 
 CREATE TABLE IF NOT EXISTS icona
 (
-    nome VARCHAR(50) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     percorso VARCHAR(255) NOT NULL,
     dim_x INT NOT NULL,
     dim_y INT NOT NULL,
-    nome_set_icone VARCHAR(50) NOT NULL,
-    PRIMARY KEY (nome, percorso),
+    nome_set_icone VARCHAR(100) NOT NULL,
+    PRIMARY KEY (nome),
     FOREIGN KEY (nome_set_icone) REFERENCES set_icone(nome)
 );
 
 CREATE TABLE IF NOT EXISTS sfida
 (
-    data DATE NOT NULL,
-    ora TIME NOT NULL,
+    id SERIAL,
+    data_sfida DATE NOT NULL,
+    ora_sfida TIME NOT NULL,
     moderata BOOLEAN,
     durata_max INTERVAL NOT NULL,
-    num_dadi INT NOT NULL,
+    max_squadre INT NOT NULL,
     id_gioco SERIAL,
-    PRIMARY KEY (data, ora),
+    CHECK (max_squadre >= 2),
+    PRIMARY KEY (id),
     FOREIGN KEY (id_gioco) REFERENCES gioco(id)
 );
 
 CREATE TABLE IF NOT EXISTS squadra
 (
     nome VARCHAR(50) NOT NULL,
-    data_sfida DATE NOT NULL,
-    ora_sfida TIME NOT NULL,
     punteggio INT NOT NULL,
-    nome_icona VARCHAR(50) NOT NULL,
-    percorso_icona VARCHAR(255) NOT NULL,
-    PRIMARY KEY (nome, data_sfida, ora_sfida),
-    FOREIGN KEY (data_sfida, ora_sfida) REFERENCES sfida(data, ora),
-    FOREIGN KEY (nome_icona, percorso_icona) REFERENCES icona(nome, percorso),
-    UNIQUE (nome)
-);
-
-CREATE TABLE IF NOT EXISTS dado
-(
-    id SERIAL,
-    valore_min INT NOT NULL,
-    valore_max INT NOT NULL,
-    nome_squadra VARCHAR(50),
-    PRIMARY KEY (id),
-    FOREIGN KEY (nome_squadra) REFERENCES squadra(nome)
+    numero_dadi INT NOT NULL,
+    valore_dadi INT NOT NULL,
+    id_sfida INT NOT NULL,
+    nome_icona VARCHAR(100) NOT NULL,
+    CHECK (valore_dadi >= 0),
+    PRIMARY KEY (nome, id_sfida, nome_icona),
+    FOREIGN KEY (id_sfida) REFERENCES sfida(id),
+    FOREIGN KEY (nome_icona) REFERENCES icona(nome)
 );
 
 CREATE TABLE IF NOT EXISTS sfondo
 (
     nome VARCHAR(50) NOT NULL,
     percorso VARCHAR(255) NOT NULL,
-    PRIMARY KEY (nome, percorso)
+    PRIMARY KEY (nome)
 );
 
 CREATE TABLE IF NOT EXISTS tabellone
 (
-    id_gioco SERIAL,
+    id_gioco INT NOT NULL,
     nome_sfondo VARCHAR(50) NOT NULL,
-    percorso_sfondo VARCHAR(255) NOT NULL,
     PRIMARY KEY (id_gioco),
-    FOREIGN KEY (nome_sfondo, percorso_sfondo) REFERENCES sfondo(nome, percorso)
+    FOREIGN KEY (id_gioco) REFERENCES gioco(id),
+    FOREIGN KEY (nome_sfondo) REFERENCES sfondo(nome)
 );
 
 CREATE TABLE IF NOT EXISTS podio
@@ -91,10 +90,13 @@ CREATE TABLE IF NOT EXISTS gradino
     id_podio SERIAL,
     pos_x INT NOT NULL,
     pos_y INT NOT NULL,
-    nome_squadra VARCHAR(50) NOT NULL,
+    nome_squadra VARCHAR(50),
+    id_sfida INT,
+    nome_icona VARCHAR(100),
+    CHECK (numero BETWEEN 1 AND 3),
     PRIMARY KEY (numero, id_podio),
     FOREIGN KEY (id_podio) REFERENCES podio(id_tabellone),
-    FOREIGN KEY (nome_squadra) REFERENCES squadra(nome)
+    FOREIGN KEY (nome_squadra, id_sfida, nome_icona) REFERENCES squadra(nome, id_sfida, nome_icona)
 );
 
 CREATE TABLE IF NOT EXISTS giocatore
@@ -135,20 +137,21 @@ CREATE TABLE IF NOT EXISTS video
 (
     nome VARCHAR(50) NOT NULL,
     percorso VARCHAR(255) NOT NULL,
-    PRIMARY KEY (nome, percorso)
+    PRIMARY KEY (nome)
 );
 
 CREATE TABLE IF NOT EXISTS task
 (
-    id SERIAL,
+    id SERIAL NOT NULL,
+    testo VARCHAR(1000) NOT NULL,
     tempo_max INTERVAL NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS risposta_task
 (
-    id SERIAL,
-    id_task SERIAL,
+    id SERIAL NOT NULL,
+    id_task INT NOT NULL,
     testo VARCHAR(1000) NOT NULL,
     punteggio INT NOT NULL,
     percorso_file VARCHAR(255),
@@ -166,48 +169,48 @@ CREATE TABLE IF NOT EXISTS immagine
 (
     nome VARCHAR(50) NOT NULL,
     percorso VARCHAR(255) NOT NULL,
-    PRIMARY KEY (nome, percorso)
+    PRIMARY KEY (nome)
 );
 
 CREATE TABLE IF NOT EXISTS quiz
 (
     id SERIAL,
+    testo VARCHAR(1000) NOT NULL,
     tempo_max INTERVAL NOT NULL,
     nome_immagine VARCHAR(50),
-    percorso_immagine VARCHAR(255),
     PRIMARY KEY (id),
-    FOREIGN KEY (nome_immagine, percorso_immagine) REFERENCES immagine(nome, percorso)
+    FOREIGN KEY (nome_immagine) REFERENCES immagine(nome)
 );
 
 CREATE TABLE IF NOT EXISTS risposta_quiz
 (
     id SERIAL,
-    id_quiz SERIAL,
+    id_quiz INT NOT NULL,
     testo VARCHAR(1000) NOT NULL,
     punteggio INT NOT NULL,
     corretta BOOLEAN NOT NULL,
     nome_immagine VARCHAR(50),
-    percorso_immagine VARCHAR(255),
     PRIMARY KEY (id, id_quiz),
     FOREIGN KEY (id_quiz) REFERENCES quiz(id),
-    FOREIGN KEY (nome_immagine, percorso_immagine) REFERENCES immagine(nome, percorso)
+    FOREIGN KEY (nome_immagine) REFERENCES immagine(nome)
 );
 
 CREATE TABLE IF NOT EXISTS casella
 (
     num_ord INT NOT NULL,
-    id_gioco SERIAL,
+    id_gioco INT NOT NULL,
     pos_x INT NOT NULL,
     pos_y INT NOT NULL,
     lancio_dadi BOOLEAN NOT NULL,
-    id_tabellone SERIAL,
+    id_tabellone INT NOT NULL,
     nome_video VARCHAR(50),
-    percorso_video VARCHAR(255),
-    id_task SERIAL,
-    id_quiz SERIAL,
+    id_task INT,
+    id_quiz INT,
+    -- CHECK ((id_task IS NOT NULL) AND (id_quiz IS NOT NULL)),
     PRIMARY KEY (num_ord, id_gioco),
+    FOREIGN KEY (id_gioco) REFERENCES gioco(id),
     FOREIGN KEY (id_tabellone) REFERENCES tabellone(id_gioco),
-    FOREIGN KEY (nome_video, percorso_video) REFERENCES video(nome, percorso),
+    FOREIGN KEY (nome_video) REFERENCES video(nome),
     FOREIGN KEY (id_task) REFERENCES task(id),
     FOREIGN KEY (id_quiz) REFERENCES quiz(id)
 );
@@ -238,21 +241,11 @@ CREATE TABLE IF NOT EXISTS giocatore_appartiene_squadra
 (
     email_giocatore VARCHAR(50) NOT NULL,
     nome_squadra VARCHAR(50) NOT NULL,
+    id_sfida INT NOT NULL,
+    nome_icona VARCHAR(100) NOT NULL,
     PRIMARY KEY (email_giocatore, nome_squadra),
     FOREIGN KEY (email_giocatore) REFERENCES giocatore(email),
-    FOREIGN KEY (nome_squadra) REFERENCES squadra(nome)
-);
-
-CREATE TABLE IF NOT EXISTS squadra_partecipa_sfida
-(
-    nome_squadra VARCHAR(50) NOT NULL,
-    data_sfida DATE NOT NULL,
-    ora_sfida TIME NOT NULL,
-    min_squadre INT NOT NULL,
-    max_squadre INT NOT NULL,
-    PRIMARY KEY (nome_squadra, data_sfida, ora_sfida),
-    FOREIGN KEY (nome_squadra) REFERENCES squadra(nome),
-    FOREIGN KEY (data_sfida, ora_sfida) REFERENCES sfida(data, ora)
+    FOREIGN KEY (nome_squadra, id_sfida, nome_icona) REFERENCES squadra(nome, id_sfida, nome_icona)
 );
 
 CREATE TABLE IF NOT EXISTS giocatore_fornisce_risposta_task
